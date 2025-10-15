1ac67e958c96bf27b5957a1b109a4ce6577eeead
Bruno Costa <bhcosta90@gmail.com>
2025-10-15 17:19:52 -0300

    add model reference for Tenant in configuration

--- config/tenant.php
+++ config/tenant.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 return [
+    'model' => QuantumTecnology\Tenant\Models\Tenant::class,
     'table' => [
         'progress' => env('TENANT_TABLE_PROGRESS', 'tenant_migrations_progress'),
     ],
